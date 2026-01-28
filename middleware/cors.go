c87cadad9b2da7e4463f95faff3c2b66ee757966
FaridAlvaro <dekfarid2015@gmail.com>
2026-01-28 08:39:03 +0700

    cors FE

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,7 +10,7 @@ import (
 func CorsMiddleware() fiber.Handler {
 	allowedOrigins := os.Getenv("CORS_ORIGINS")
 	if allowedOrigins == "" {
-		allowedOrigins = "https://frontend-mbg.vercel.app,https://backendmbg-production.up.railway.app,http://localhost:5000,https://localhost:5000,http://localhost:5173,https://localhost:5173"
+		allowedOrigins = "https://frontend-mbg.vercel.app,https://frontend-manbig.vercel.app,https://backendmbg-production.up.railway.app,http://localhost:5000,https://localhost:5000,http://localhost:5173,https://localhost:5173"
 	}
 
 	return cors.New(cors.Config{
